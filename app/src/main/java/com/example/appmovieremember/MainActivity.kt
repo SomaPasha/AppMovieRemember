74cf8fc119459a1defafd512ffb2a98506f67dd5
pavel <pavel.kuzneczov.96@gmail.com>
2023-06-14 01:16:47 +0500

    binding and easy toolbar

--- app/src/main/java/com/example/appmovieremember/MainActivity.kt
+++ app/src/main/java/com/example/appmovieremember/MainActivity.kt
@@ -1,11 +1,20 @@
 package com.example.appmovieremember
 
+import android.app.ActionBar
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Toolbar
+import com.example.appmovieremember.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityMainBinding
+    //private  lateinit var toolbar: androidx.appcompat.widget.Toolbar;
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+        setSupportActionBar(binding.commonWindowToolBar)
+       // supportActionBar?.setDisplayHomeAsUpEnabled(true)
     }
 }
